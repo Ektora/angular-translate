c05dbbd22b704f4c42368c8353e7ee147057b014
Ektora <zsoltlucski@gmail.com>
2022-05-08 13:48:56 +0200

    Váz kialakítása

---
+++ src/app/translate/translate.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateComponent } from './translate/translate.component';
+
+
+
+@NgModule({
+  declarations: [
+    TranslateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TranslateModule { }
